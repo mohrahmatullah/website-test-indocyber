b89f7beaa09929016fcf5c65212e2adb679edc58
mohrahmatullah <mohrahmatullah17051992@gmail.com>
2020-11-05 15:11:19 +0700

    commit

--- application/views/home/detail-product.php
+++ application/views/home/detail-product.php
@@ -9,20 +9,7 @@
   <div class="container">
 
     <div class="row">
-
-      <div class="col-lg-3">
-
-        <h1 class="my-4">Categories</h1>
-        <div class="list-group">
-          <a href="#" class="list-group-item">Category 1</a>
-          <a href="#" class="list-group-item">Category 2</a>
-          <a href="#" class="list-group-item">Category 3</a>
-        </div>
-
-      </div>
-      <!-- /.col-lg-3 -->
-
-      <div class="col-lg-9">
+      <div class="col-lg-12">
 
         <div class="col-lg-9">
 
@@ -54,12 +41,13 @@
           <!-- /.card -->
 
         </div>
-
-        </div>
         <!-- /.row -->
 
       </div>
       <!-- /.col-lg-9 -->
+      
+
+      
 
     </div>
     <!-- /.row -->
