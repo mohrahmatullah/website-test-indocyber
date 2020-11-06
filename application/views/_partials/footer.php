e7fe73d6ea54c7724f8635cb31d15edf236f24eb
mohrahmatullah <mohrahmatullah17051992@gmail.com>
2020-11-06 09:19:53 +0700

    commit

--- application/views/_partials/footer.php
+++ application/views/_partials/footer.php
@@ -1,15 +1,54 @@
 <!-- Footer -->
-  <footer class="py-5 bg-dark" style="margin-top: 35rem;">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Shop 2020</p>
-    </div>
-    <!-- /.container -->
-  </footer>
+	<footer class="py-5 bg-dark" style="margin-top: 35rem;">
+		<div class="container">
+		  <p class="m-0 text-center text-white">Copyright &copy; Shop 2020</p>
+		</div>
+	<!-- /.container -->
+	</footer>
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="<?php echo base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
-  <script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-
+	<!-- Bootstrap core JavaScript -->
+	<script src="<?php echo base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+	<!-- Import Sweetalert -->
+	<script src="<?php echo base_url('assets/sweetalert/sweetalert.min.js'); ?>"></script>
+	<script type="text/javascript">
+	    $(".remove-cart").click(function(){
+        	const id_cart = $(this).data('id');
+        	const id_produk = $(this).data('id_produk');
+        	const qty = $(this).data('qty');
+	    
+			swal({
+				title: "Are you sure?",
+				text: "You will not be able to recover this imaginary file!",
+				type: "warning",
+				showCancelButton: true,
+				confirmButtonClass: "btn-danger",
+				confirmButtonText: "Yes, delete it!",
+				cancelButtonText: "No, cancel plx!",
+				closeOnConfirm: false,
+				closeOnCancel: false
+			},
+			function(isConfirm) {
+				if (isConfirm) {
+				  $.ajax({
+				     url: '/delete-cart/'+id_cart+'/'+id_produk+'/'+qty,
+				     type: 'DELETE',
+				     error: function() {
+				        alert('Something is wrong');
+				     },
+				     success: function(data) {
+				          // $("#"+id).remove();
+				          swal("Deleted!", "Your imaginary file has been deleted.", "success");
+				     }
+				  });
+				} else {
+				  swal("Cancelled", "Your imaginary file is safe :)", "error");
+				}
+			});
+	     
+	    });
+	    
+	</script>
 </body>
 
 </html>
--- application/views/_partials/head.php
+++ application/views/_partials/head.php
@@ -16,4 +16,7 @@
   <!-- Custom styles for this template -->
   <link href="<?php echo base_url('assets/css/shop-homepage.css'); ?>" rel="stylesheet">
 
+  <!-- Import Sweetalert -->
+  <link href="<?php echo base_url('assets/sweetalert/sweetalert.css'); ?>" rel="stylesheet">
+
 </head>
